12e1e78830d68bb9c8cbf55a6cfd2b2bab1a88cc
champix56 <desorbaix@free.fr>
2021-05-26 15:53:15 +0200

    data

---
+++ js/data.js
@@ -0,0 +1,21 @@
+var messages=[
+    {id:0, value:'Mon message 1', userId:0,color:'#452141', dateTime:new Date()},
+    {id:1, value:'Mon message 2', userId:2,color:'#452141', dateTime:new Date()},
+    {id:2, value:'Mon message 3', userId:1,color:'#452141', dateTime:new Date()},
+    {id:3, value:'Mon message 4', userId:7,color:'#452141', dateTime:new Date()},
+    {id:4, value:'Mon message 5', userId:3,color:'#452141', dateTime:new Date()},
+    {id:5, value:'Mon message 6', userId:5,color:'#452141', dateTime:new Date()},
+    {id:6, value:'Mon message 7', userId:3,color:'#452141', dateTime:new Date()},
+    {id:7, value:'Mon message 8', userId:6,color:'#452141', dateTime:new Date()}
+];
+var users=[
+    {id:0, nom:'Alex', img:'https://blog.orsys.fr/les-carnets/wp-content/uploads/2016/05/DESORBAIX-e1463732091296@2x.png'},
+    {id:1, nom:'Mathieu', img:'https://cdn.zmescience.com/wp-content/uploads/2018/05/169764_web.jpg'},
+    {id:2, nom:'André', img:'https://i.pinimg.com/originals/97/e4/2a/97e42a82fc7911961d3ca55f54d1372c.jpg'},
+    {id:3, nom:'Yann', img:'https://www.byrdie.com/thmb/kAXw1ZXSfixyaiIjLt0tVfRzhhs=/843x1024/filters:fill(auto,1)/GettyImages-1035308964-5d15a77127854247b5c841d339a546c0.jpeg'},
+    {id:4, nom:'Matthieu', img:'https://i.pinimg.com/originals/2a/be/5e/2abe5e5aadcc214d5af693d366ec4f82.jpg'},
+    {id:5, nom:'Leo', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPTxvu0n1Q8htjLwCHp3IrgqwkkjzpekrTMtk82wHjVJlCjn7A&s'},
+    {id:6, nom:'Thomas', img:'https://images.unsplash.com/photo-1619182597083-17bda72c1d56?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fG1hbiUyMGZhY2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'},
+    {id:7, nom:'Damase', img:'https://i.pinimg.com/originals/94/6e/82/946e829a135f68d7a041e3a83b445f55.jpg'}
+
+];
